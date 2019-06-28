11d93584aa2157c17188fc44beeaede0c3dfd4a4
Vova Kurylo <voky191@gmail.com>
2019-06-28 16:23:57 +0300

    added full images and sizes

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $products = json_decode($result, true);
 
 $db = Db::getConnection();
 
-for ($i=0;$i<32;$i++)
+for ($i=0;$i<10;$i++)
 {
     $product = $products['data'][$i];
 
@@ -29,25 +29,19 @@ for ($i=0;$i<32;$i++)
         $price_usd = $product['current_price_usd'];
         //var_dump($sku);
 
+        $sizes = $product['sizes_eu'];
 
-        $sql = 'INSERT INTO product (id, name, brand, sku, thumbnail_url, price_usd) VALUES (:id,:name,:brand,:sku,:thumbnail_url,:price_usd)';
+        $sizes_string = implode(' , ', $sizes);
+
+
+
+        $sql = 'INSERT INTO product (id, name, brand, sku, sizes, price_usd) VALUES (:id,:name,:brand,:sku,:sizes,:price_usd)';
         $result = $db->prepare($sql);
         $result->bindParam(':id', $id, PDO::PARAM_STR);
         $result->bindParam(':name', $name, PDO::PARAM_STR);
         $result->bindParam(':brand', $brand, PDO::PARAM_STR);
         $result->bindParam(':sku', $sku, PDO::PARAM_STR);
-
-        if (!file_exists('images/' . $brand)) {
-        mkdir('images/' . $brand, 0777, true);
-        }
-
-        if($thumbnail_url != '') {
-        $url = $thumbnail_url;
-        $img = 'images/' . $brand . '/' . $sku . '.jpg';
-        file_put_contents($img, file_get_contents($url));
-        }
-
-        $result->bindParam(':thumbnail_url', $img, PDO::PARAM_STR);
+        $result->bindParam(':sizes', $sizes_string, PDO::PARAM_STR);
         $result->bindParam(':price_usd', $price_usd, PDO::PARAM_STR);
 
         $result->execute();
@@ -65,6 +59,56 @@ for ($i=0;$i<32;$i++)
         $db->query($sql);
         $color_id = $db->lastInsertId();
         $sql = "INSERT INTO product_color(product_id, color_id) VALUES ('$id','$color_id')";
+        $db->query($sql);
+    }
+
+        $innerUrl = "https://engine.sneakers123.com/api/releases/".$product['slug'];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $innerUrl);
+        $result=curl_exec($ch);
+        curl_close($ch);
+
+        $sneakers = json_decode($result, true);
+
+        $brand = $sneakers['brand'];
+        $sku = $sneakers['sku'];
+        $id = $sneakers['id'];
+
+        $img = $sneakers['image'];
+        $gallery = $sneakers['gallery'];
+        $prod = $sneakers['products'];
+
+    $img_array = [];
+    $img_array[0] = $img;
+    $img_array[1] = $gallery[0];
+    $img_array[2] = $gallery[1];
+
+
+    if (!file_exists('images/' . $brand)) {
+        mkdir('images/' . $brand, 0777, true);
+    }
+
+    if (!file_exists('images/' . $brand . '/' . $sku)) {
+        mkdir('images/' . $brand. '/' . $sku, 0777, true);
+    }
+
+    if($img != '') {
+        $url = $img;
+        $img = 'images/' . $brand . '/' . $sku . '/'.$sku.'-1.jpg';
+        file_put_contents($img, file_get_contents($url));
+    }
+
+    foreach($gallery as $key => $value){
+        $url2 = $value;
+        $path = 'images/' . $brand . '/' . $sku . '/'.$sku.'-'.($key+2).'.jpg';
+        file_put_contents($path, file_get_contents($url2));
+    }
+
+    foreach($img_array as $key => $value){
+        $path = 'images/' . $brand . '/' . $sku . '/'.$sku.'-'.($key+1).'.jpg';
+        $sql = "INSERT INTO images(product_id, image) VALUES ('$id','$path')";
         $db->query($sql);
     }
 
